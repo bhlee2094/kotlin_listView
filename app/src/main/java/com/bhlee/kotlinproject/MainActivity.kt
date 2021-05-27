c293b67093d7ed9e6f168cd64305cb4fbfd74977
bh lee <djbh123@naver.com>
2021-05-27 14:35:08 +0900

    navigationView

--- app/src/main/java/com/bhlee/kotlinproject/MainActivity.kt
+++ app/src/main/java/com/bhlee/kotlinproject/MainActivity.kt
@@ -2,11 +2,14 @@ package com.bhlee.kotlinproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.AdapterView
 import android.widget.Toast
+import androidx.core.view.GravityCompat
 import com.bhlee.kotlinproject.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     var UserList = arrayListOf<User>(
         User(R.drawable.android, "bhlee", "26", "안녕하세요"),
@@ -23,6 +26,12 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.btnNavi.setOnClickListener {
+            binding.layoutDrawer.openDrawer(GravityCompat.START)
+        }
+
+        binding.naviView.setNavigationItemSelectedListener(this)
+
         val item = arrayOf("사과", "배", "딸기", "키위", "포도")
 
 //        binding.listView.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, item)
@@ -32,6 +41,24 @@ class MainActivity : AppCompatActivity() {
         binding.listView.onItemClickListener = AdapterView.OnItemClickListener {parent, view, position, id ->
             val selectItem = parent.getItemAtPosition(position) as User
             Toast.makeText(this, selectItem.name, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.access -> Toast.makeText(applicationContext, "접근성", Toast.LENGTH_SHORT).show()
+            R.id.email -> Toast.makeText(applicationContext, "이메일", Toast.LENGTH_SHORT).show()
+            R.id.message -> Toast.makeText(applicationContext, "메시지", Toast.LENGTH_SHORT).show()
+        }
+        binding.layoutDrawer.closeDrawers()
+        return false
+    }
+
+    override fun onBackPressed() {
+        if(binding.layoutDrawer.isDrawerOpen(GravityCompat.START)){
+            binding.layoutDrawer.closeDrawers()
+        }else{
+            super.onBackPressed()
         }
     }
 }
